efd2a164cf98c3d862052e0ef0b4a0d571818238
Lee Iverson <leei@ece.ubc.ca>
2009-02-18 12:38:18 -0800

    Don't translate :multi-valued facets in Facet#value.
    In order to process facet value/counts for :multi-valued facets, one must not attempt to translate them, otherwise we'll end up with one of the object.send()s in #translate returning an Array of values.  A much simpler solution is to just use the attribute_value directly.  Unfortunately, we're then left only being able to use integral :multi values in our facets.

--- lib/thinking_sphinx/facet.rb
+++ lib/thinking_sphinx/facet.rb
@@ -1,19 +1,19 @@
 module ThinkingSphinx
   class Facet
     attr_reader :reference
-    
+
     def initialize(reference)
       @reference = reference
-      
+
       if reference.columns.length != 1
         raise "Can't translate Facets on multiple-column field or attribute"
       end
     end
-    
+
     def name
       reference.unique_name
     end
-    
+
     def attribute_name
       @attribute_name ||= case @reference
       when Attribute
@@ -22,12 +22,12 @@ module ThinkingSphinx
         @reference.unique_name.to_s + "_sort"
       end
     end
-    
+
     def value(object, attribute_value)
       return translate(object, attribute_value) if @reference.is_a?(Field)
-      
+
       case @reference.type
-      when :string, :multi
+      when :string
         translate(object, attribute_value)
       when :datetime
         Time.at(attribute_value)
@@ -37,20 +37,20 @@ module ThinkingSphinx
         attribute_value
       end
     end
-    
+
     def to_s
       name
     end
-    
+
     private
-    
+
     def translate(object, attribute_value)
       column.__stack.each { |method|
         object = object.send(method)
       }
       object.send(column.__name)
     end
-    
+
     def column
       @reference.columns.first
     end
